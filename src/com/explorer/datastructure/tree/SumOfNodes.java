87aa58bc02e52a6aa0a7d07c14bff3a9dc5e2a96
shivang.goel <shivang.goel@irdeto.com>
2023-09-30 19:27:47 +0530

    Added level order sum using BFS

---
+++ src/com/explorer/datastructure/tree/SumOfNodes.java
@@ -0,0 +1,108 @@
+package com.explorer.datastructure.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.explorer.datastructure.tree.model.TreeNode;
+
+public class SumOfNodes {
+
+  public static void main(String[] args) {
+    // Example Usage
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(2);
+    root.right = new TreeNode(3);
+    root.left.left = new TreeNode(4);
+    root.left.right = new TreeNode(5);
+    root.right.left = new TreeNode(8);
+    SumOfNodes sumOfNodes = new SumOfNodes();
+    System.out.println("sumOfNodes = " + sumOfNodes.sumOfNodes(root));
+    System.out.println("maxLevelSum = " + sumOfNodes.maxLevelSum(root));
+  }
+
+  private int sumOfNodes(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int leftSum = sumOfNodes(root.left);
+    int rightSum = sumOfNodes(root.right);
+    return leftSum + rightSum + root.val;
+  }
+
+  /**
+   * Maximum Level Sum of a Binary Tree
+   *
+   * Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+   * Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+   *
+   * https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/description/
+   *
+   * Solved by level order or BFS
+   *
+   * @param root
+   * @return
+   */
+  private int maxLevelSum(TreeNode root) {
+    if(root == null){
+      return 1;
+    }
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(root);
+    queue.add(null);
+
+    int maxSum = Integer.MIN_VALUE;
+    int lSum = 0;
+    int maxLevel = 1;
+    int curLevel = 1;
+
+    while(!queue.isEmpty()){
+      TreeNode curNode = queue.remove();
+      if(curNode == null){
+        if(lSum > maxSum){
+          maxSum = lSum;
+          maxLevel = curLevel;
+        }
+        lSum = 0;
+        curLevel++;
+        if(queue.isEmpty()){
+          break;
+        }else{
+          queue.add(null);
+        }
+      }else {
+        lSum = lSum + curNode.val;
+        if(curNode.left != null){
+          queue.add(curNode.left);
+        }
+        if(curNode.right != null){
+          queue.add(curNode.right);
+        }
+      }
+    }
+    return maxLevel;
+  }
+
+  /**
+   * Sum Root to Leaf Numbers
+   * You are given the root of a binary tree containing digits from 0 to 9 only.
+   *
+   * Each root-to-leaf path in the tree represents a number.
+   *
+   * For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+   * Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+   *
+   * A leaf node is a node with no children.
+   *
+   * https://leetcode.com/problems/sum-root-to-leaf-numbers/
+   *
+   * Solved by DFS
+   * @param root
+   * @return
+   */
+  private int sumNumbers(TreeNode root) {
+    // to be done using DFS
+    return 0;
+  }
+}
