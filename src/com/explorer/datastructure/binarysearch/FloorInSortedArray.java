c04318e8d0831d18d74c72a96939c8c2347a3ca1
shivang.goel <shivang.goel@irdeto.com>
2025-02-03 18:05:49 +0530

    Add FloorInSortedArray

--- src/com/explorer/datastructure/binarysearch/FloorInSortedArray.java
+++ src/com/explorer/datastructure/binarysearch/FloorInSortedArray.java
@@ -1,5 +1,28 @@
 package com.explorer.datastructure.binarysearch;
 
+/**
+ * Floor in a Sorted Array
+ *
+ * Given a sorted array arr[] (with unique elements) and an integer k, find the index (0-based) of the largest element in arr[] that is less than or equal to k. This element is called the "floor" of k. If such an element does not exist, return -1.
+ *
+ * Examples
+ *
+ * Input: arr[] = [1, 2, 8, 10, 11, 12, 19], k = 0
+ * Output: -1
+ * Explanation: No element less than 0 is found. So output is -1.
+ * Input: arr[] = [1, 2, 8, 10, 11, 12, 19], k = 5
+ * Output: 1
+ * Explanation: Largest Number less than 5 is 2 , whose index is 1.
+ * Input: arr[] = [1, 2, 8], k = 1
+ * Output: 0
+ * Explanation: Largest Number less than or equal to  1 is 1 , whose index is 0.
+ * Constraints:
+ * 1 ≤ arr.size() ≤ 106
+ * 1 ≤ arr[i] ≤ 106
+ * 0 ≤ k ≤ arr[n-1]
+ *
+ * https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
+ */
 public class FloorInSortedArray {
 
   public static void main(String[] args) {
